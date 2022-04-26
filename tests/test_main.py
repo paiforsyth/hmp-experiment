8eea5295f1c410e2f6e17f589b7ac33344910737
paiforsyth <paiforsyth@hotmail.com>
2022-04-26 10:45:59 -0400

    add torch test

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,6 +3,7 @@ import pytest
 from click.testing import CliRunner
 
 from hmp_experiment import __main__
+import torch
 
 
 @pytest.fixture
@@ -15,3 +16,9 @@ def test_main_succeeds(runner: CliRunner) -> None:
     """It exits with a status code of zero."""
     result = runner.invoke(__main__.main)
     assert result.exit_code == 0
+
+
+def test_can_use_torch():
+    A=torch.tensor([1.0])
+    B = torch.tensor([2.0])
+    torch.testing.assert_allclose(A, B/2)
